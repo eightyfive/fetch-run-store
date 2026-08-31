10039c8068665fe0a6c05a5ed4d0f6ab7e0baae0
0x55 <0xfiftyfive@gmail.com>
2026-08-31 23:55:04 +0200

    fix: encode route parameters

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export function buildRoute(route: string, routeParams: RouteParams) {
   let url = route;
 
   for (const [name, value] of Object.entries(routeParams)) {
-    url = url.replace(`:${name}`, `${value}`);
+    url = url.replace(`:${name}`, encodeURIComponent(String(value)));
   }
 
   return url;
